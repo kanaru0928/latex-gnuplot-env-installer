d70ac90656c99dee77b5270d3cb800f87ac41f6a
kanaru0928 <kanaru0928@gmail.com>
2024-04-04 12:20:32 +0000

    first commit

---
+++ install-gnuplot-linux.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "管理者権限を付けて実行してください(sudo)。"
+  exit 1
+fi
+
+existsCommand () {
+  command -v $1 > /dev/null 2>&1
+}
+
+if ! existsCommand "kpsewhich"; then
+  echo "TeX Liveがインストールされていません。"
+  exit 1
+fi
+
+if ! existsCommand "gnuplot"; then
+  echo "gnuplotがインストールされていません。"
+  exit 1
+fi
+
+echo "スタイルファイルのインストール中"
+
+cd $(kpsewhich -var-value=TEXMFLOCAL)
+sudo mkdir gnuplot
+cd gnuplot
+sudo gnuplot -e "set term tikz createstyle"
+
+if [ ! -f /usr/share/gnuplot/gnuplot/*/lua/gnuplot-tikz.lua ]; then
+  echo "luaの設定ファイルが存在しません。gnuplotが壊れている可能性があります。gnuplotを再インストールしてください。"
+  exit 1
+fi
+
+echo "luaの設定を変更中"
+sudo sed -i -E '/^ *notimestamp *=/s/false/true/' /usr/share/gnuplot/gnuplot/*/lua/gnuplot-tikz.lua
+
+echo "gnuplot設定完了"
---
+++ install-gnuplot-windows.ps1
@@ -0,0 +1,34 @@
+#Requires -RunAsAdministrator
+
+function Find-Executable (
+  [string] $command
+) {
+  $null -ne (Get-Command -Name $command -ErrorAction SilentlyContinue)
+}
+
+if (! Find-Executable "kpsewhich") {
+  echo "TeX Liveがインストールされていません。"
+  throw
+}
+
+if (! Find-Executable "gnuplot") {
+  echo "gnuplotがインストールされていません。"
+  throw
+}
+
+echo "スタイルファイルのインストール中"
+$texmfPath = kpsewhich -var-value=TEXMFLOCAL
+cd $texmfPath
+mkdir gnuplot
+cd gnuplot
+gnuplot -e "set term tikz createstyle"
+
+if (! Test-Path "C:\Program Files\gnuplot\share\lua\gnuplot-tikz.lua") {
+  echo "luaの設定ファイルが存在しません。gnuplotが壊れている可能性があります。gnuplotを再インストールしてください。"
+}
+
+echo "luaの設定を変更中"
+$luaFile = "C:\Program Files\gnuplot\share\lua\gnuplot-tikz.lua"
+(Get-Content $luaFile) | $_ -replace "^ *notimestamp = false,$" "  notimestamp = true," | Set-Content $luaFile
+
+echo "gnuplot設定完了"
