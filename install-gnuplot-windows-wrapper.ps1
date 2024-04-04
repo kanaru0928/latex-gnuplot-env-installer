51b50c382fcd4aee333f946275817b910e595f18
kanaru0928 <kanaru0928@gmail.com>
2024-04-04 12:26:39 +0000

    update wrapper

--- install-gnuplot-windows-wrapper.ps1
+++ install-gnuplot-windows-wrapper.ps1
@@ -1,7 +1,7 @@
 #Requires -RunAsAdministrator
 
-$scriptURL = "https://e-chan1007.github.io/setup-uec-paper-scripts/windows.ps1"
-$scriptPath = "$env:TEMP/setup-uec-paper-scripts.ps1"
+$scriptURL = "https://kanaru0928.github.io/latex-gnuplot-env-installer/install-gnuplot-windows.ps1"
+$scriptPath = "$env:TEMP/setup-gnu-plot-env-scripts.ps1"
 
 Start-BitsTransfer -Source "$scriptURL" -Destination "$scriptPath"
 powershell -ExecutionPolicy Bypass -File "$scriptPath"
